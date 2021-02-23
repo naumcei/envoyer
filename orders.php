20a8c1667a1557685b6ef8551d984b1567a3beb3
Naumce Ivanovski <naumce.ivanovski@gmail.com>
2021-02-23 18:11:46 +0100

    [ECM-e] - order-page-php  - Order page php support

---
+++ orders.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+class User
+{
+    // What the heck is 7 for?
+    public $access = 7;
+
+    public function parse(string $code): void
+    {
+        $tokens = $this->tokenizer->tokenize($code);
+        $ast = $this->lexer->lexify($tokens);
+        foreach ($ast as $node) {
+            // parse...
+        }
+    }
+}
+
+// What the heck is 4 for?
+if ($user->access & 4) {
+    // ...
+}
+
+// What's going on here?
+$user->access ^= 2;
+
+function isShopOpen($day): bool
+{
+    if ($day) {
+        if (is_string($day)) {
+            $day = strtolower($day);
+            if ($day === 'friday') {
+                return true;
+            } elseif ($day === 'saturday') {
+                return true;
+            } elseif ($day === 'sunday') {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+    return false;
+}
