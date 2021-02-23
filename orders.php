ac9cef075192c2092fd0dd0255865d5362ba2366
Naumce Ivanovski <naumce.ivanovski@gmail.com>
2021-02-23 23:57:00 +0100

    [ECM-3] - order-page-php  - rulset.xml

--- orders.php
+++ orders.php
@@ -1,33 +1,7 @@
 <?php
 
 declare(strict_types=1);
-
-class User
-{
-    // What the heck is 7 for?
-    public $access = 7;
-
-    //TODO
-    public function parse(string $code): void
-    {
-        $tokens = $this->tokenizer->tokenize($code);
-        $ast = $this->lexer->lexify($tokens);
-        foreach ($ast as $node) {
-            // parse...
-            //must have this
-        }
-    }
-
-    //I need to implement new feature plus another one
-}
-
-// What the heck is 4 for?
-if ($user->access & 4) {
-    // ...
-}
-
-// What's going on here?
-$user->access ^= 2;
+$a=3;
 
 function isShopOpen($day): bool
 {
